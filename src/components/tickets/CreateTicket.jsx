07b6f1b087fb61d7c71eab3a2f6c305a405093b1
cnfairall <101303134+cnfairall@users.noreply.github.com>
2024-02-03 14:29:35 -0600

    add ticket

--- src/components/tickets/CreateTicket.jsx
+++ src/components/tickets/CreateTicket.jsx
@@ -1,3 +1,109 @@
+import Form from "react-bootstrap/Form";
+import { getCustomers } from "../../data/customerData";
+import { getEmployees } from "../../data/employeeData";
+import { useEffect, useState } from "react";
+import { Button } from "reactstrap";
+import { createTicket } from "../../data/serviceTicketsData";
+
+const initialState = {
+  customerId: -1,
+  employeeId: -1,
+  description: "",
+  emergency: false,
+};
+
 export default function CreateTicket() {
-  return <h3>Submit a Ticket</h3>;
-}
+  const [customers, setCustomers] = useState([]);
+  const [employees, setEmployees] = useState([]);
+  const [formInput, setFormInput] = useState(initialState);
+
+  useEffect(() => {
+    getEmployees().then(setEmployees);
+    getCustomers().then(setCustomers);
+    // if (ticketObj.id) setFormInput(ticketObj);
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // if (ticketObj.id) {
+    //   updateTicket(formInput);
+    // } else {
+      const payload = formInput;
+      createTicket(payload);
+    }
+
+    return (
+      <Form onSubmit={handleSubmit}>
+        <Form.Select
+          name="customerId"
+          className="mb-3"
+          required
+          aria-label="customer id select"
+          onChange={handleChange}
+          value={formInput.customerId}
+          >
+          <option value="">Select Customer Id</option>
+          {customers.map((c) => (
+            <option
+              key={`customer-${c.id}`}
+              value={c.id}
+            >
+              {c.id} : {c.name}
+            </option>
+          ))}
+        </Form.Select>
+        <Form.Select
+          name="employeeId"
+          className="mb-3"
+          aria-label="employee id select"
+          value={formInput.employeeId}
+          onChange={handleChange}
+        >
+          <option value="">Select Employee Id</option>
+          {employees.map((e) => (
+            <option
+              key={`employee-${e.id}`}
+              value={e.id}
+            >
+              {e.id} : {e.name}
+            </option>
+          ))}
+        </Form.Select>
+        <Form.Group className="mb-3">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            value={formInput.description}
+            onChange={handleChange}
+            as="textarea"
+            rows={3}
+            name="description"
+            required
+          />
+        </Form.Group>
+        <Form.Check
+          className="mb-3" // prettier-ignore
+          name="emergency"
+          type="switch"
+          id="emergency-bool"
+          label="Check if emergency"
+          checked={formInput.emergency}
+          onChange={(e) => {
+            setFormInput((prevState) => ({
+              ...prevState,
+              emergency: e.target.checked,
+            }));
+          }}
+        />
+        <Button type="submit">Submit</Button>
+      </Form>
+    );
+  };
--- src/components/tickets/ServiceTickets.jsx
+++ src/components/tickets/ServiceTickets.jsx
@@ -1,10 +1,9 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 export default function ServiceTickets() {
   return (
     <>
       <h2>Service Tickets</h2>
-      <Link to="/tickets/create">Add</Link>
       <Outlet />
     </>
   );
